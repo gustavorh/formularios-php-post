faac72a6984164b71f9d46f2d1e538ad6e02d8c4
Gustavo <gustavocars@gmail.com>
2022-10-01 21:31:05 -0300

    Act.

---
+++ noticias.php
@@ -0,0 +1,10 @@
+<?php
+
+# Policial
+$lineas = file("policial.txt");
+for ($i = 0; $i < 2; $i++) {
+    #$lineas = str_replace(" ", "<br>", $lineas);
+    echo $lineas[i];
+}
+
+?>
--- ver_noticias.php
+++ ver_noticias.php
@@ -60,11 +60,11 @@
                     <div class="card">
                       <div class="card-body">
                         <h5 class="card-title text-center">Policial</h5>
-                        <h6 class="card-subtitle mb-2 text-muted">Titulo</h6>
-                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <h6 class="card-subtitle mb-2 text-muted"><?php include "noticias.php"; ?></h6>
+                        <p class="card-text">Cuerpo</p>
                       </div>
                       <div class="card-footer">
-                        <small class="text-muted">Last updated 3 mins ago</small>
+                        <small class="text-muted">Fecha</small>
                       </div>
                     </div>
                     <div class="card">
